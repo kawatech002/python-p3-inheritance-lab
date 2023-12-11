a475ad2c632a48de48e9c21b7368eb82fade45d8
kawatech002 <kawanjiku94@gmail.com>
2023-12-11 08:45:55 +0300

    complete lab

--- lib/student.py
+++ lib/student.py
@@ -1,8 +1,11 @@
-#!/usr/bin/env python
-
 from user import User
 
 class Student(User):
+    def __init__(self, first_name, last_name, knowledge=[]):
+        super().__init__(first_name, last_name)
+        
+        self.knowledge = knowledge
+
     
-    def learn(self):
-        pass
+    def learn(self, knowledge):
+        self.knowledge.append(knowledge)
